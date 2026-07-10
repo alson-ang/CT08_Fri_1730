90c07a088410ec7a18c6dca29419a8ce281a5730
alson-ang <alson.ang@computhink.com.sg>
2026-07-10 10:57:41 +0000

    Jul 10, 2026, 10:57 AM

--- basic.js
+++ basic.js
@@ -58,7 +58,7 @@ function setup() {
     groceries.push("orange")    // Add to end of array (like append)
     groceries.push("butter")
     groceries.shift()
-    groceries.splice(1, 1, "kaya")  // (start index, )
+    groceries.splice(1, 1, "kaya")  // (start index, items to delete, value to add)
 }
 
 function draw() {
