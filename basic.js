89f5a5b5b4447cb39de45168d38a97d07867a8eb
alson-ang <alson.ang@computhink.com.sg>
2026-07-10 10:37:54 +0000

    Jul 10, 2026, 10:37 AM

--- basic.js
+++ basic.js
@@ -8,8 +8,8 @@ function setup() {
     let b = 10;
     let sum = a + b;
     let product = a * b;
-    const pi = 3.14159
-    const gravity = 9.8
+    const pi = 3.14159;
+    const gravity = 9.8;
     console.log("Sum of a and b:", sum);
     console.log("Product of a and b:", product);
 
