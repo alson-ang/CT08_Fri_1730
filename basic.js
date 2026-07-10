40a1f2874667c0112efec42cd474cb3a4aafcb2d
alson-ang <alson.ang@computhink.com.sg>
2026-07-10 10:23:50 +0000

    Jul 10, 2026, 10:23 AM

--- basic.js
+++ basic.js
@@ -22,9 +22,7 @@ function setup() {
     console.log("Area:", area);
     text(area, 100, 100);
 
-    for (let i = 0; i < 10; i += 2) {
-        console.log(i)
-    }
+    for (let i = 0; i < 10; i += 2) { console.log(i);}
 }
 
 function draw() {
