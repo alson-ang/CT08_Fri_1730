b26b35347d0bdbc84cbc33e0ec7aa1fcf68fe94c
alson-ang <alson.ang@computhink.com.sg>
2026-07-17 10:50:25 +0000

    Jul 17, 2026, 10:50 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -1,3 +1,5 @@
+const { startTransition } = require("react");
+
 //player box
 let player; // Player sprite
 let box;    // Player sprite image
@@ -36,7 +38,8 @@ function setup() {
 
     // Spawn point [x, y]
     startCoordinate = [50, height - TILE_SIZE / 2];
-    player.x = startCoordinate[0]
+    player.x = startCoordinate[0];
+    player.y = startCoordinate[1];
 }
 
 function draw() {
