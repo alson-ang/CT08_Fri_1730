7fc09893111229d376795f9a3ee2a9929b48ef94
alson-ang <alson.ang@computhink.com.sg>
2026-07-24 10:14:06 +0000

    Jul 24, 2026, 10:14 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -26,7 +26,7 @@ function preload() {
     box = loadImage("assets/cube.png");
     bg = loadImage("assets/geobg.png");
     tileMap1 = loadStrings("stages/tiles1.txt");
-    
+    spike = loadImage("assets/spike.png");
 }
 
 function setup() {
