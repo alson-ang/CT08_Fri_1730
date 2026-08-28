34608f4fffc6fdb14fab44be2e4e2cd35eaecca0
alson-ang <alson.ang@computhink.com.sg>
2026-08-28 10:17:44 +0000

    Aug 28, 2026, 10:17 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -57,7 +57,7 @@ function draw() {
 
         // Loop through all detected hands
         for (let i = 0; i < hands.length; i++) {
-            
+            let hand = 
         }
         let keypoint = hands
         circle(200, 200, 10);   // (x pos, y pos, diameter)
