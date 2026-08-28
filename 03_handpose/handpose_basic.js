5b448c19cf30cc77e9f3c77f15290271e0d2b445
alson-ang <alson.ang@computhink.com.sg>
2026-08-28 09:53:18 +0000

    Aug 28, 2026, 9:53 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -42,6 +42,7 @@ function setup() {
     video = createCapture(constraints);
     video.size(videoW, videoH);
     video.hide();
+    // Send video to the model to 
     handPose.detectStart(video, gotHands);
 }
 
