d9932e55b5b797174c75fe206cd4b82b5c9caec3
alson-ang <alson.ang@computhink.com.sg>
2026-08-28 09:46:27 +0000

    Aug 28, 2026, 9:46 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -34,7 +34,9 @@ function setup() {
                 minHeight: videoH
             },
             optional: [{ minFrameRate: 60 }],
-        }
+        },
+        audio: false,
+        
     }
 }
 
