5efded2a6ca98eec431d3d766527a45056e0c730
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:29:59 +0000

    Sep 11, 2026, 10:29 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -110,7 +110,7 @@ function draw() {
     // Show start menu
     if (gameStart === false) {
         textSize(40);
-        textAlign(CENTER, CENTER); 
+        textAlign(CENTER, CENTER); // horizontal & vertical alignment
         fill("rgb(0, 255, 20)")
         text("Bounce the Ball", width / 2, height / 2);// text(string, x, y)
     }
