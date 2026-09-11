0f9477594e1666c3b51dc58662414a0d3fc0e90f
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:26:44 +0000

    Sep 11, 2026, 10:26 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -110,7 +110,7 @@ function draw() {
     // Show start menu
     if (gameStart === false) {
         textSize(40);
-        fill("255")
+        fill("rgb(255, 0, 0)")
         // text(string, x, y)
     }
 
