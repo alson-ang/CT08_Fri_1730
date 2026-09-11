efdcb6f17fc5226380e41db45b2e824be64bf503
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:32:39 +0000

    Sep 11, 2026, 10:32 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -118,7 +118,7 @@ function draw() {
         // Instructions
         textSize(32);
         fill("rgb(0, 200, 20)");
-        text("Use your Index Finger to bounce the Ball", width / 2, height / 2);
+        text("Use your Index Finger to Bounce the Ball", width / 2, height / 2);
     }
 
     // Check if model detects a hand
