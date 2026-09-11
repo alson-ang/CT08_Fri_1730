586d460504f85f8ed80dbcee0bf17a435df78838
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:25:12 +0000

    Sep 11, 2026, 10:25 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -109,7 +109,7 @@ function draw() {
 
     // Show start menu
     if (gameStart === false) {
-        // text(text, x, y)
+        // text(string, x, y)
     }
 
     // Check if model detects a hand
