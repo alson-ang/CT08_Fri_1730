b2c85f4116c3010f76b6ad9690627fa95e7a0dd6
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:03:39 +0000

    Sep 11, 2026, 10:03 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -70,7 +70,7 @@ function setup() {
     balloon.drag = 0.3;
 
     leftWall = new Sprite();
-    leftWall.x = 1;
+    leftWall.x = 0;
     leftWall.y = 1;
     leftWall.width = 1;
     leftWall.height = 1;
