38118f7209b63d2aa6f127ce1175b6d2f24ad1be
alson-ang <alson.ang@computhink.com.sg>
2026-09-11 10:57:12 +0000

    Sep 11, 2026, 10:57 AM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -137,7 +137,7 @@ function draw() {
             fingerTip.x = keypoint.x;
             fingerTip.y = keypoint.y;
             fingerTip.visible = true;
-            fingerTip.collider = "none";
+            fingerTip.collider = "kinematic";
         } else {
             // Hide sprite if there are no hands
             fingerTip.visible = false;
